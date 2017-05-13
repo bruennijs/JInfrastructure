30d39df7ac5339259c02f035c51703d70307130c
Olli B <oliver.bruentje@gmx.de>
2017-05-13 13:27:08 +0200

    Added tests to find items in collections

--- de.bruenni.infrastructure.common/src/test/java/org/hamcrest/collection/IsIterableEquivalentTest.java
+++ de.bruenni.infrastructure.common/src/test/java/org/hamcrest/collection/IsIterableEquivalentTest.java
@@ -1,9 +1,13 @@
 package org.hamcrest.collection;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.springframework.data.domain.ExampleMatcher;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,10 +25,43 @@ public class IsIterableEquivalentTest {
 	}
 
 	@Test
-	public void when_size_equal_expect_matches_succeeds() throws Exception {
+	public void IsIterableContaining_expect_is_like_collectionequivalent() throws Exception {
 		List<String> actual = Arrays.asList("b", "a");
 
-		Assert.assertThat(actual, IsIterableEquivalent.to("a", "b"));
-		Assert.assertThat(actual, IsIterableEquivalent.to(Arrays.asList("a", "b")));
+		Assert.assertThat(actual, Matchers.containsInAnyOrder(Arrays.asList(IsEqual.equalTo("a"), IsEqual.equalTo("b"))));
+		Assert.assertThat(actual, IsIterableContainingInAnyOrder.containsInAnyOrder(Arrays.asList(IsEqual.equalTo("a"), IsEqual.equalTo("b"))));
+
+		Assert.assertThat(actual, IsNot.not(Matchers.contains(Arrays.asList(IsEqual.equalTo("a"), IsEqual.equalTo("b")))));
+
+		// in order
+		Assert.assertThat(actual, Matchers.contains(Arrays.asList(IsEqual.equalTo("b"), IsEqual.equalTo("a"))));
+	}
+
+	@Test
+	public void IsIterableContaining_expect_is_like_collectionequivalent_with_specific_matcher() throws Exception {
+		List<String> actual = Arrays.asList("ab", "ac");
+
+		Assert.assertThat(actual, Matchers.containsInAnyOrder(Arrays.asList(
+			StringContains.containsString("a"),
+			StringContains.containsString("a"))));
+	}
+
+	@Test
+	public void IsCollectionContaining_test() throws Exception {
+		List<String> actual = Arrays.asList("ab", "ac");
+
+		Assert.assertThat(actual, IsCollectionContaining.hasItem(StringContains.containsString("a")));
+		Assert.assertThat(actual, IsCollectionContaining.hasItem(StringContains.containsString("b")));
+		Assert.assertThat(actual, IsCollectionContaining.hasItem(StringContains.containsString("c")));
+		Assert.assertThat(actual, IsNot.not(IsCollectionContaining.hasItem(StringContains.containsString("d"))));
+	}
+
+	@Test
+
+	public void IsIterableContaining_expect_understand_them_correclty_3() throws Exception {
+		List<String> actual = Arrays.asList("ab", "ac");
+
+		Assert.assertThat(actual, IsNot.not(Matchers.containsInAnyOrder(Arrays.asList(StringContains.containsString("a")))));
 	}
 }
+
